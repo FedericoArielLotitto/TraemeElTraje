62aaff8028d365907d3538a4e39877e5a69c99c9
Federico Ariel Lotitto <federicoariellotitto@gmail.com>
2021-09-26 01:13:25 -0300

    Agrega index.html

---
+++ pasoUno.js
@@ -0,0 +1,24 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      brand: 'Traeme el traje',
+      opcionUno: 'Mis pedidos',
+      tituloUno: 'Datos de contacto',
+      tituloNombre: 'Nombre',
+      tituloApellido: 'Apellido',
+      tituloTelefono: 'Teléfono de contacto',
+      mensajeDeAdvertencia: 'Ingrese el número empezando con 11',
+      tituloDos: 'Talles',
+      tituloCuello: 'Contorno de cuello',
+      tituloPecho: 'Contorno de pecho',
+      tituloCintura: 'Contorno de cintura',
+      tituloCorreoElectronico: 'Correo electrónico',
+      nombre: '',
+      apellido: '',
+      telefono: '',
+      correoElectronico: '',
+      nombre: '',
+      nombreReceptor: '',
+      cantidad: 0
+    }
+});
