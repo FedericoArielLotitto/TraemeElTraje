46edab9785fc893b3129cb92c795aaa221658306
Federico Ariel Lotitto <federicoariellotitto@gmail.com>
2021-10-11 00:09:30 -0300

    Agrega font-awesome

--- navbar/navbar.js
+++ navbar/navbar.js
@@ -11,9 +11,9 @@ Vue.component('nav-bar', {
         `<nav id="navbar" class="fondo-navbar navbar navbar-expand-md">
             <div class="container-fluid">
                 <a class="navbar-brand color-texto-navbar" :href="home">{{ brand }}</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+            <button class="navbar-toggler light" type="button" data-bs-toggle="collapse"
                 data-bs-target="#opcionesDePedido">
-                <span class="navbar-toggler-icon"></span>
+                <span><i class="fas fa-bars menu-icon"></i></span>
             </button>
             <div class="collapse navbar-collapse" id="opcionesDePedido">
                 <div class="navbar-nav">
